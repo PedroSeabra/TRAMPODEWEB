897b62120429b884b38d47327c5db4ed47cdee23
pczfelipe <felipeamorim42apple@hotmail.com>
2018-06-26 16:27:45 -0300

    .

--- liga.php
+++ liga.php
@@ -1,30 +1,22 @@
-<html>
-    <head>
-        <title> registrando liga</title>
-    </head>
-    <body>
-    <?php
-        session_start();
+<?php
+    session_start();
         
-        //conecting:
-        $db = mysqli_connect("127.0.0.1", "root", "12345", "bd_ufsbola");
+    //conecting:
+    $db = mysqli_connect("localhost", "root", "02091999", "bd_ufsbola");
         
-        if(isset($_POST['criar'])){
+    if(isset($_POST['btn_criar'])){
             
-            //session_start();
-            $nome = mysqli_real_escape_string($db, $_POST['nomeLiga']);
-            $npartici = mysqli_real_escape_string($db, $_POST['numLiga']);
-            $descri = mysqli_real_escape_string($db, $_POST['descLiga']);
-            $pontuacao30 = mysqli_real_escape_string($db, $_POST['pontuacao1']);
-            $pontuacao20 = mysqli_real_escape_string($db, $_POST['pontuacao2']);
-            $pontuacao15 = mysqli_real_escape_string($db, $_POST['pontuacao3']);
-            $pontuacao5 = mysqli_real_escape_string($db, $_POST['pontuacao4']);
+        //session_start();
+        $nome = mysqli_real_escape_string($db, $_POST['nomeLiga']);
+        $npartici = mysqli_real_escape_string($db, $_POST['numLiga']);
+        $descri = mysqli_real_escape_string($db, $_POST['descLiga']);
+        $pontuacao30 = mysqli_real_escape_string($db, $_POST['pontuacao1']);
+        $pontuacao20 = mysqli_real_escape_string($db, $_POST['pontuacao2']);
+        $pontuacao15 = mysqli_real_escape_string($db, $_POST['pontuacao3']);
+        $pontuacao5 = mysqli_real_escape_string($db, $_POST['pontuacao4']);
         
-            $sql = "INSERT INTO tb_ligas(nome_usuarios, nome_ligas, maxparticipantes_ligas, descricao_ligas, pontosplacarexato_ligas, pontosvencedor_ligas, pontosplacarnaoexato_ligas, pontosgol_ligas) VALUES('$nome', '$npartici', '$descri', '$pontuacao30', '$pontuacao20', '$pontuacao15', '$pontuacao5')";
-            $_SESSION['message'] = "Cadastro de liga realizado com sucesso";
-            
-        }
-    ?>
-    
-    </body>
-</html>
+        $sql = "INSERT INTO tb_ligas(nome_ligas, maxparticipantes_ligas, descricao_ligas, pontosplacarexato_ligas, pontosvencedor_ligas, pontosplacarnaoexato_ligas, pontosgol_ligas) VALUES('$nome', '$npartici', '$descri', '$pontuacao30', '$pontuacao20', '$pontuacao15', '$pontuacao5')";
+        $_SESSION['message'] = "Cadastro de liga realizado com sucesso";
+        
+    }
+?>
